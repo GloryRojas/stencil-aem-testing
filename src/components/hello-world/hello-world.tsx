ed1f4a0482ac03c4d4bce854888aecc569a27131
Glory Rojas <grojasm94@gmail.com>
2022-12-06 00:38:59 -0500

    first commit

---
+++ src/components/hello-world/hello-world.tsx
@@ -0,0 +1,31 @@
+import {Component, Host, h, Prop} from '@stencil/core';
+
+@Component({
+  tag: 'hello-world',
+  styleUrl: 'hello-world.css',
+  shadow: true,
+})
+export class HelloWorld {
+  @Prop() text: string;
+
+  @Prop() message: string;
+
+  render() {
+    return (
+      <Host>
+        <div class="cmp-helloworld">
+          <h2 class="cmp-helloworld__title">Hello, World!</h2>
+          <div class="cmp-helloworld__item">
+            <p class="cmp-helloworld__item-label">Text property:</p>
+            <pre class="cmp-helloworld__item-output" >I'm {this.text}</pre>
+          </div>
+          <div class="cmp-helloworld__item" >
+            <p class="cmp-helloworld__item-label">Model message:</p>
+            <pre class="cmp-helloworld__item-output">{this.message}</pre>
+          </div>
+        </div>
+      </Host>
+    );
+  }
+
+}
---
+++ src/components/hello-world/readme.md
@@ -0,0 +1,18 @@
+# hello-world
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property  | Attribute | Description | Type     | Default     |
+| --------- | --------- | ----------- | -------- | ----------- |
+| `message` | `message` |             | `string` | `undefined` |
+| `text`    | `text`    |             | `string` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
